3aebe9a97efc3a7a7fc89b0359afbfe1d6a692be
franco <franquicidad@gmail.com>
2020-12-17 16:29:52 -0500

    navigation bug

--- app/src/main/java/com/franco/rickandmortymvvmapp/MainActivity.kt
+++ app/src/main/java/com/franco/rickandmortymvvmapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,32 +15,33 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    var navController: NavController? = null
     private val viewModel:MainViewModel by viewModels()
-
-
-
-    //private val customFragmentFactory =CustomFragmentFactory(Repository)
     override fun onCreate(savedInstanceState: Bundle?) {
-       // supportFragmentManager.fragmentFactory =
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment,HomeFragment::class.java,null)
-            .commit()
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.nav_host_fragment,HomeFragment::class.java,null)
+//            .commit()
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                        R.id.navigation_home,
+                        R.id.navigation_dashboard,
+                        R.id.navigation_notifications
+                )
+        )
         setupActionBarWithNavController(navController!!, appBarConfiguration)
         navView.setupWithNavController(navController!!)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController?.navigateUp()!!
     }
 }
--- app/src/main/java/com/franco/rickandmortymvvmapp/di/AppModule.kt
+++ app/src/main/java/com/franco/rickandmortymvvmapp/di/AppModule.kt
@@ -1,8 +1,11 @@
 package com.franco.rickandmortymvvmapp.di
 
 import com.franco.rickandmortymvvmapp.data.database.LocalDataSource
+import com.franco.rickandmortymvvmapp.data.database.LocalDataSourceImpl
+import com.franco.rickandmortymvvmapp.data.domain.Repository
 import com.franco.rickandmortymvvmapp.data.domain.RepositoryImpl
 import com.franco.rickandmortymvvmapp.data.network.RemoteDataSource
+import com.franco.rickandmortymvvmapp.data.network.RemoteDataSourceImpl
 import com.franco.rickandmortymvvmapp.ui.home.HomeViewModel
 import dagger.Module
 import dagger.Provides
@@ -16,10 +19,21 @@ object AppModule {
 
     @Singleton
     @Provides
+    fun provideLocalDataSource():LocalDataSource{
+        return LocalDataSourceImpl()
+    }
+  @Singleton
+    @Provides
+    fun provideRemoteDataSource():RemoteDataSource{
+        return RemoteDataSourceImpl()
+    }
+
+    @Singleton
+    @Provides
     fun provideRepository(
         localDataSource: LocalDataSource,
         remoteDataSource: RemoteDataSource
-    ): RepositoryImpl {
+    ): Repository {
         return RepositoryImpl(localDataSource, remoteDataSource)
     }
 
